d2d369066e56f3a293fcfc980db92530c92e4677
chreid1973 <chreid@gmail.com>
2025-10-18 19:25:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-// Shim so Render starts even if it insists on `node index.js`
-try {
-  require('./index.cjs');
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
+try { require("./index.cjs"); }
+catch (e) { console.error(e); process.exit(1); }
